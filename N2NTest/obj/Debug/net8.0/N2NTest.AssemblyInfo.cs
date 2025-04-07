8b0214d782628a5a15863121989d631af8096605
AdminoBratt <sigge.bratt1@gmail.com>
2025-04-07 20:44:02 +0200

    working

--- N2NTest/obj/Debug/net8.0/N2NTest.AssemblyInfo.cs
+++ N2NTest/obj/Debug/net8.0/N2NTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("N2NTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239d90b98650bef96dd230b29a84df9346b35415")]
 [assembly: System.Reflection.AssemblyProductAttribute("N2NTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("N2NTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
